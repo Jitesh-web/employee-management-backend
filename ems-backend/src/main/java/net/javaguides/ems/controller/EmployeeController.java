f48dca5743f9e79b2b3ae11feca5018cc8ae11b2
Jitesh <jiteshnarode@gmail.com>
2026-07-10 16:48:53 +0530

    Added OpenAPI Swagger documentation for employee management system

--- ems-backend/src/main/java/net/javaguides/ems/controller/EmployeeController.java
+++ ems-backend/src/main/java/net/javaguides/ems/controller/EmployeeController.java
@@ -1,5 +1,9 @@
 package net.javaguides.ems.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import net.javaguides.ems.dto.EmployeeDto;
@@ -12,7 +16,11 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-//Adding cross origin to all the frontend to make api call to avoid CROS erro
+//Adding cross origin to all the frontend to make api call to avoid CROS error
+@Tag(
+        name = "Employee Management",
+        description = "REST APIs for Employee Management"
+)
 @CrossOrigin("*")
 @AllArgsConstructor
 @RestController
@@ -25,6 +33,16 @@ public class EmployeeController {
     private EmployeeService employeeService;
 
     //Create a new employee
+    @Operation(
+            summary = "Create Employee",
+            description = "Creates a new employee and stores it in the database."
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Employee created successfully"
+            )
+    })
     @PostMapping
     public ResponseEntity<EmployeeDto> createEmployee(@Valid @RequestBody EmployeeDto employeeDto) {
         LOGGER.info("Received request to create employee");
@@ -32,25 +50,66 @@ public class EmployeeController {
         LOGGER.info("Employee created successfully with id: {}", savedEmployee.getId());
         return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
     }
+
     //Get a employee by Id
+    @Operation(
+            summary = "Get Employee by ID",
+            description = "Retrieves an employee using the employee ID."
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Employee found successfully"
+            )
+    })
     @GetMapping("/{Id}")
     public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("Id") Long employeeId) {
         EmployeeDto employee = employeeService.getEmployeeById(employeeId);
         return new ResponseEntity<>(employee, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Get all Employees",
+            description = "Retrieves all employees."
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Employee retrieved successfully"
+            )
+    })
     @GetMapping
     public ResponseEntity<List<EmployeeDto>> getAllEmployees() {
         List<EmployeeDto> employees = employeeService.getAllEmployees();
         return new ResponseEntity<>(employees, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Update Employee by ID",
+            description = "Updating employee by using employee ID."
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Employee updated successfully"
+            )
+    })
     @PutMapping("/{Id}")
     public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable("Id") Long employeeId, @Valid @RequestBody EmployeeDto employeeDto) {
         EmployeeDto employee = employeeService.updateEmployee(employeeId, employeeDto);
         return ResponseEntity.ok(employee);
     }
 
+    @Operation(
+            summary = "Delete Employee by ID",
+            description = "Delete an employee using the employee ID."
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Employee deleted successfully"
+            )
+    })
     @DeleteMapping("/{Id}")
     public ResponseEntity<String> deleteEmployeeById(@PathVariable("Id") Long employeeId) {
         employeeService.deleteEmployeeById(employeeId);
